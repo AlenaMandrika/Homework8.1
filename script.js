4b8869ab0ddb3a220cee7c702daa2093b2cdb57a
alenamandrika <Alenenok0@ukr.net>
2017-12-14 00:21:17 +0200

    add item movement

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+// e.clientX, e.clientY координати курсора в момент кліка відносно вікна
+// elem.getBoundingClientRect() вертає розмір елемента і позицію відносно вікна
+// e.offsetX, e.offsetY зміщення по х курсора миші відносно початку координат елемента
+// clientTop товщина верхньої границі елемента в пікселях
+// clientHeight,clientWidth внутрішня висота/ширина елемента в пікселях
 
+var elem = false;
 var blockBtn = document.getElementById('blockBtn');
-blockBtn.addEventListener('click', function (e) {
-    var elem = e.target;
+
+blockBtn.addEventListener('mousedown', function (e) {
+    elem = e.target;
     if (elem && elem.id === 'blockBtn') {
 
         var newDiv = document.createElement('div');
         newDiv.classList.add('block');
+
+        var coorNewDiv = newDiv.getBoundingClientRect();
+        newDiv.style.left = e.clientX - coorNewDiv.width / 2 - 30 + 'px';
+        newDiv.style.top = e.clientY - coorNewDiv.height / 2 - 20 + 'px';
 
         var newBtn = document.createElement('button');
         newBtn.innerHTML = 'lorem';
@@ -16,6 +27,9 @@ blockBtn.addEventListener('click', function (e) {
     }
 
     if (elem && elem.className === 'loremBtn') {
+
+        event.preventDefault();
+
         var activeClass = elem.classList.contains('active');
         if (!activeClass) {
             elem.classList.add('active');
@@ -31,42 +45,54 @@ blockBtn.addEventListener('click', function (e) {
         elem.parentNode.remove(elem)
     }
 
-}, false);
+    newDiv.addEventListener('mousedown', function (e) {
+        event.preventDefault();
+        elem = true;
+
+        e.offsetX = newDiv.offsetLeft - e.clientX;
+        e.offsetY = newDiv.offsetTop - e.clientY;
+
+        newDiv.addEventListener('mousemove', function (e) {
+
+            var coorBlockBtn = blockBtn.getBoundingClientRect();
+            console.log(coorBlockBtn);
+            blockBtn.style.top = coorNewDiv.top + blockBtn.clientTop + 'px';
+            blockBtn.style.left = coorNewDiv.left + blockBtn.clientLeft + 'px';
+
+            var newCoordNewDiv = {
+                top: e.clientY - coorBlockBtn.top - newDiv.clientHeight / 2,
+                left: e.clientX - coorBlockBtn.left - newDiv.clientWidth / 2
+            };
+
+            if (elem) {
+                if (newCoordNewDiv.top < 0) { //верх
+                    newCoordNewDiv.top = 0;
+                }
+                if (newCoordNewDiv.left < 0) { //лівий
+                    newCoordNewDiv.left = 0;
+
+                    newDiv.children[1].style.float = 'right'
+                }
+                if (newCoordNewDiv.left + newDiv.clientWidth > blockBtn.clientWidth) { // правий
+                    newCoordNewDiv.left = blockBtn.clientWidth - newDiv.clientWidth;
+                    newDiv.children[1].style.float = 'left'
+                }
+                if (newCoordNewDiv.top + newDiv.clientHeight > blockBtn.clientHeight) { //низ
+                    newCoordNewDiv.top = blockBtn.clientHeight - newDiv.clientHeight;
+                }
+
+                newDiv.style.left = newCoordNewDiv.left + 'px'; //нові координати
+                newDiv.style.top = newCoordNewDiv.top + 'px';
+            }
 
 
-blockBtn.addEventListener('mousedown', function (e) {
-    var elem = e.target;
-    dragableElem = true;
-    var coorNewDiv = elem.getBoundingClientRect();
-    console.log(coorNewDiv);
+        }, false);
 
-    elem.style.top = coorNewDiv.top+'px';//початкові координати
-    elem.style.left = coorNewDiv.left+'px';
+        newDiv.addEventListener('mouseup', function () {
+            elem = false;
+        }, false);
 
-
-
-
-}, false);
-
-blockBtn.addEventListener('mouseup', function (e) {
-    dragableElem = false;
-
-});
-
-blockBtn.addEventListener('mousemove', function (e) {
-    var elem = e.target;
-
-    var x = e.clientX; //координата відносно вікна
-    var y = e.clientY;
-
-    //var maxWinX = document.getElementById('blockBtn').offsetWidth; //ширина контейнера
-    //var maxWinY = document.getElementById('blockBtn').offsetHeight;
-
-    var osx = e.offsetX;//зміщення по х курсора миші
-    var osy = e.offsetY;
-
-    elem.style.left = x - osx +'px';
-    elem.style.top = y - osy +'px';
+    }, false);
 
 
 }, false);
